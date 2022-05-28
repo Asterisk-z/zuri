7a852ad1ff26c4912e11b95545e6eb532d0650c2
Olang Daniel <olangdan17@gmail.com>
2022-05-28 07:46:09 +0100

    reading text

--- python/Reading-Text-Files/main.py
+++ python/Reading-Text-Files/main.py
@@ -3,14 +3,27 @@
 # count_words("The cake is done. It is a big cake!") 
 # --> {"cake":2, "big":1, "is":2, "the":1, "a":1, "it":1}
 
+from collections import Counter
+
+
 def read_file_content(filename):
     # [assignment] Add your code here 
+
+    with open(filename) as text:
+        lines = text.read()
+        text.close()
     
-    return "Hello World"
+    return lines.strip('?.').split()
 
 
 def count_words():
     text = read_file_content("./story.txt")
     # [assignment] Add your code here
 
-    return {"as": 10, "would": 20}
+    count = Counter(text)
+
+    return count
+
+
+
+count_words()
