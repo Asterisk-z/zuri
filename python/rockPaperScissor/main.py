6857ee42fff466fb8c01e6f87d1554b27311a1d9
Olang Daniel <olangdan17@gmail.com>
2022-06-19 06:46:49 +0100

    rockpaperscissor

--- python/rockPaperScissor/main.py
+++ python/rockPaperScissor/main.py
@@ -2,12 +2,9 @@ import random
 
 options = ["R", "P", "S"]
 optionsFull = {"R":"ROCK", "P":"PAPER", "S":"SCISSOR"}
-# global game
-# global mySelectionNumber
-# global aiSelectionNumber
-# game = 0
-# mySelectionNumber = 0
-# aiSelectionNumber = 0
+scoreBoard = {"Player": 0, "AI": 0 , "TIE": 0}
+
+
 
 print("Welcome to Rock Paper Scissor Game \n")
 print("You will be playing against an formidable AI \n BEST OF LUCK")
@@ -18,9 +15,6 @@ print("WHERE S STANCE FOR SCISSOR")
 
 def main():
 
-    game = 0
-    mySelectionNumber = 0
-    aiSelectionNumber = 0
     mySelection = str(input("Enter your selection :")).upper()
 
     if ((mySelection == "R") or (mySelection == "P") or (mySelection == "S") ):
@@ -29,53 +23,50 @@ def main():
 
         print("Your Selection is", optionsFull[mySelection])
         print("AI Selection is", optionsFull[aiSelection])
-        print("present score = Player(",optionsFull[mySelection],") : CPU (",optionsFull[aiSelection],")")
 
         if (mySelection == aiSelection):
             print("Ouch! \n lets go another round...")
-            game = game + 1
-            if (game == 3):
+            scoreBoard["TIE"] += 1
+            main()
+            if (scoreBoard["TIE"] == 3):
                 print("It a tie! \n lets go another round...")
                 exit()
         
-        if (((optionsFull[mySelection]) == "Rock") and ((optionsFull[aiSelection]) == "Scissor")):
+        if ((optionsFull[mySelection] == "ROCK") and (optionsFull[aiSelection] == "SCISSOR")):
            playerpoint()
-        if (((optionsFull[mySelection]) == "Paper") and ((optionsFull[aiSelection]) == "Rock")):
+        if ((optionsFull[mySelection] == "PAPER") and (optionsFull[aiSelection] == "ROCK")):
            playerpoint()
-        if (((optionsFull[mySelection]) == "Scissor") and ((optionsFull[aiSelection]) == "Paper")):
+        if ((optionsFull[mySelection] == "SCISSOR") and (optionsFull[aiSelection] == "PAPER")):
            playerpoint()
 
-        if (((optionsFull[aiSelection]) == "Rock") and ((optionsFull[mySelection]) == "Scissor")):
+        if ((optionsFull[aiSelection] == "ROCK") and (optionsFull[mySelection] == "SCISSOR")):
            aipoint()
-        if (((optionsFull[aiSelection]) == "Paper") and ((optionsFull[mySelection]) == "Rock")):
+        if ((optionsFull[aiSelection] == "PAPER") and (optionsFull[mySelection] == "ROCK")):
            aipoint()
-        if (((optionsFull[aiSelection]) == "Scissor") and ((optionsFull[mySelection]) == "Paper")):
+        if ((optionsFull[aiSelection] == "SCISSOR") and (optionsFull[mySelection] == "PAPER")):
            aipoint()
-        
 
-        print("Finally you made a selection !!!")
-        exit()
+
+        print("present score = Player(",scoreBoard["Player"],") : CPU (",scoreBoard["AI"],")")
+
+        main()
         
 
     print("Error, Please Select Between P, R, S !!!")
     main()
-    # print(mySelection)
-    # print(aiSelection)
 
 def playerpoint():
-    mySelectionNumber += 1
-    if (mySelectionNumber == 3):
+    scoreBoard["Player"] += 1
+    if (scoreBoard["Player"] == 3):
         print("You Are the Winner")
         exit()
     print("One Point Added to you, Lets Play another round")
-    main()
  
 def aipoint():
-    aiSelectionNumber += 1
-    if (aiSelectionNumber == 3):
+    scoreBoard["AI"] += 1
+    if (scoreBoard["AI"] == 3):
         print("AI is the Winner")
         exit()
     print("One Point Added to you, Lets Play another round")
-    main()
 
 main()
