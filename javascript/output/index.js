5ce2ed69d45355ff67decee2b717dc8422e9c901
Olang Daniel <olangdan17@gmail.com>
2022-06-24 14:28:28 +0100

    js

---
+++ javascript/output/index.js
@@ -0,0 +1,10 @@
+const name = "Olang Daniel";
+const height = 5.9;
+const country = "Nigeria";
+
+
+console.log("Name : " + name);
+console.log("Height :  " + height);
+console.log("Country : " + country);
+
+window.alert("hello");
